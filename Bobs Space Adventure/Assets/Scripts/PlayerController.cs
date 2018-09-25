cb84d8de82fbc020bc1d2086797b6122a0d9a699
Goggon <bettemoller@hotmail.com>
2018-09-25 19:50:29 +0200

    simpel Player controller med collision
    Player kan bevæges og top og bottom væg smadrer player ved collision

--- Bobs Space Adventure/Assets/Scripts/PlayerController.cs
+++ Bobs Space Adventure/Assets/Scripts/PlayerController.cs
@@ -6,16 +6,56 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    
+    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
+
+
     private Vector3 Spawnpos = new Vector3(-9, 3.5F, 0);
+    public float MoveSpeed;
+
+    public bool invert = true;
+    public bool begin = true;
+
+    public GameObject Player;
 
 	// Use this for initialization
 	void Start () {
         this.transform.position = (Spawnpos);
-	}
+
+        rb2d = GetComponent<Rigidbody2D>();       
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown("space") || Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("test");
+
+            Vector2 move = new Vector2(0, MoveSpeed);
+
+            if (begin == true)
+            {
+                rb2d.AddForce(move * -1);
+                invert = false;
+                begin = false;
+            }
+            else if (invert == false)
+            {
+                rb2d.AddForce(move * 2);
+                invert = true;
+            }
+            else if (invert == true)
+            {
+                rb2d.AddForce(move * -2);
+                invert = false;
+            }
+        }
 	}
+
+    void OnCollisionEnter2D(Collision2D objectYouCollidedWith)
+    {
+        if (objectYouCollidedWith.gameObject.name == ("South Wall") || objectYouCollidedWith.gameObject.name == ("North Wall"))
+        {
+            Destroy(Player);
+        }
+    }
 }
