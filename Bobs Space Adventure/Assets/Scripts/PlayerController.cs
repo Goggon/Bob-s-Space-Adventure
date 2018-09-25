e6aba16eea4b922b0c8195f6e6efa4b83eb65819
Goggon <bettemoller@hotmail.com>
2018-09-25 15:13:01 +0200

    Spawn position til player set
    spawner nu et bestemt sted

---
+++ Bobs Space Adventure/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour {
+
+    
+    private Vector3 Spawnpos = new Vector3(-9, 3.5F, 0);
+
+	// Use this for initialization
+	void Start () {
+        this.transform.position = (Spawnpos);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
