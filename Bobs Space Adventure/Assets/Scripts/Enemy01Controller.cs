702cb9471356cd08ac9f6b908f6bb5a1148af967
Goggon <bettemoller@hotmail.com>
2018-10-04 15:13:20 +0200

    enemy movement startup

---
+++ Bobs Space Adventure/Assets/Scripts/Enemy01Controller.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy01Controller : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    public float movespeed;
+
+    Vector2 Move;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        Vector2 Move = new Vector2(0, movespeed);
+        rb2d.velocity = (Move);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        rb2d.velocity = (Move);
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Walls")
+        {
+            rb2d.velocity = (Move * -1);
+            Debug.Log("help");
+        }
+    }
+}
