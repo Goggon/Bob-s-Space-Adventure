54a365dd50b03819da6fa1499aff4af3a42e769b
Goggon <bettemoller@hotmail.com>
2018-10-08 17:12:05 +0200

    begyndelse på enemy skud

---
+++ Bobs Space Adventure/Assets/Enemyshot.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemyshot : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+    public float speed;
+
+    public GameObject shot;
+
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.velocity = -transform.right * speed;
+    }
+
+    void OnCollisionEnter2D(Collision2D obj)
+    {
+        if (obj.gameObject.tag == "Player")
+        {
+            Destroy(shot);
+            Destroy(obj.gameObject);
+        }
+        else if (obj.gameObject.tag == "walls")
+        {
+            Destroy(shot);
+        }
+    }
+}
