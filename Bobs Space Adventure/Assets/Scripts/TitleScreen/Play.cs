6b5f39707695cc358c899d4f3c57d92050beb937
Goggon <bettemoller@hotmail.com>
2018-10-09 12:56:11 +0200

    titlescreen improvement

---
+++ Bobs Space Adventure/Assets/Scripts/TitleScreen/Play.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// Touchable component
+
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+
+public class Play : Text
+{
+    protected override void Awake()
+    {
+        base.Awake();
+    }
+}
+ 
+ // Touchable_Editor component, to prevent treating the component as a Text object.
+ 
+ 
+ 
+ [CustomEditor(typeof(Play))]
+public class Play_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        Debug.Log("xd");
+    }
+}
